15701722296eb91f6a6e0456bb81bdb366a035a0
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-03-04 20:36:55 +0600

    C#

---
+++ BolodToBoss/Algorithm/SearchAlgorithm/SearchAlgorithm/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] choukirTola = new string[6];
+
+            //choukirTola[0] = "Balish";
+            //choukirTola[1] = "Kol-Balish";
+            //choukirTola[2] = "T-Shir";
+            //choukirTola[3] = "Charger";
+            //choukirTola[4] = "Sim";
+            //choukirTola[5] = "Head-Phone";
+           
+            //for (int i = 0; i < choukirTola.Length; i++)
+            //{
+            //    var searchCharger = choukirTola[i];
+            //    if (searchCharger == "Charger")
+            //    {
+            //        Console.WriteLine("Paici charger :)");
+            //        Console.WriteLine();
+            //        break;
+            //    }
+            //}
+
+
+            string[] locationSearch= new string[5];
+            locationSearch[0] = "Cousin-House";
+            locationSearch[1] = "Home-Town";
+            locationSearch[2] = "Hotel";
+            locationSearch[3] = "Friend-House";
+            locationSearch[4] = "Town-House";
+
+            // Find Index In Array,,,,,,,,,,,,,
+
+            var index = Array.FindIndex(locationSearch, x => x == "Friend-House");
+
+            Console.WriteLine("Index= "+index);
+            for (int i = 0; i < locationSearch.Length; i++)
+            {
+                var findHouse = locationSearch[i];
+                if (findHouse == "Friend-House")
+                {
+                    Console.WriteLine("Paici :)");
+                    Console.WriteLine("Index = "+i);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ BolodToBoss/Algorithm/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] result=new int[8] {34,56,45,89,58,3,45,25};
+           
+            int temp = 0;
+            int minimum = 0;
+
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                minimum = i;
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[j] < result[minimum])
+                    {
+                        minimum = j;
+                    }
+
+                }
+                temp = result[minimum];
+                result[minimum] = result[i];
+                result[i] = temp;
+            }
+
+
+            foreach (var i in result)
+            {
+                Console.Write(i+ " ");
+            }
+            Console.WriteLine();
+
+            //Array.Sort(result);
+
+            //foreach (var i in result)
+            //{
+            //    Console.Write(i+" ");
+            //}
+          
+        }
+    }
+}
