2ec74ad637f0fde7ead81d9161af22d622f4dc7e
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-03-20 20:41:42 +0600

    C#

---
+++ ProgrammingArChoddoGusti/3rd Chapter/ReverseWord/ReverseWord/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWord
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+           string word = Console.ReadLine();
+
+           string s = ReverseString(word);
+           Console.WriteLine("Reverse Word = {0}",s);
+           Console.WriteLine();
+
+           if (word == s)
+           {
+               Console.WriteLine("Palindrome");
+           }
+           else
+           {
+               Console.WriteLine("Not palindrome");
+           }
+            Console.ReadLine();
+        }
+
+        public static string ReverseString(string str)
+        {
+            string reverseWord = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverseWord += str[i].ToString();
+            }
+            return reverseWord;
+        }
+    }
+}
---
+++ ProgrammingArChoddoGusti/3rd Chapter/SentenceReverse/SentenceReverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string reverseString = "";
+            var words = str.Split();
+
+            for (int i = words.Length-1; i >=0; i--)
+            {
+                reverseString += words[i] + " ";
+            }
+
+            Console.WriteLine("Reverse Sting = {0}",reverseString);
+            Console.ReadLine();
+        }
+
+       
+    }
+}
