cde9a38fd23dcf59948dc93161a9251fa2e039ef
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-04-01 23:18:57 +0600

    C#

---
+++ ProgrammingArChoddoGusti/6th Chapter/EvenNumberFind/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumberFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Find Number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result= EvenNumber(n);
+            Console.WriteLine(result);
+            Console.WriteLine();
+        }
+
+        public static int EvenNumber(int n)
+        {
+            if (n < 1)
+                return 1;
+            return 2*EvenNumber(n - 1);
+        }
+    }
+}
---
+++ ProgrammingArChoddoGusti/6th Chapter/FibonacciSerse/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSerse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Find Serise : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            FiboSerise(n);
+            Console.WriteLine();
+        }
+
+        public static void FiboSerise(int n)
+        {
+            Console.Write("0 1");
+            int a = 0, b = 1, c = 0;
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.Write(" " + c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
